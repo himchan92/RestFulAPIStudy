b10af8c49f35dfdd28e43f2d27dc4111c8207ec5
user <thdjun92@naver.com>
2026-04-28 22:01:53 +0900

    ex3 init

--- src/main/java/org/zerock/ex3/Ex3Application.java
+++ src/main/java/org/zerock/ex3/Ex3Application.java
@@ -2,8 +2,10 @@ package org.zerock.ex3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing //시간 자동처리 지원 필수 셋팅
 public class Ex3Application {
 
     public static void main(String[] args) {
---
+++ src/test/java/org/zerock/ex3/member/repository/MemberRepositoryTest.java
@@ -0,0 +1,33 @@
+package org.zerock.ex3.member.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+import org.zerock.ex3.member.entity.MemberEntity;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    public void testInsert() {
+        for(int i = 1; i <= 100; i++) {
+            MemberEntity memberEntity = MemberEntity.builder()
+                    .mid("user" + i)
+                    .mpw(passwordEncoder.encode("1111"))
+                    .mname("USER" + i)
+                    .email("user" + i + "@aaa.com")
+                    .role(i <= 80 ? "USER" : "ADMIN")
+                    .build();
+
+            memberRepository.save(memberEntity);
+        }
+    }
+}
