3487525adef153353d14bca7a7826829ad1fc5a9
user <thdjun92@naver.com>
2026-05-01 14:59:59 +0900

    ex3 security config

--- src/main/java/org/zerock/ex3/config/CustomSecurityConfig.java
+++ src/main/java/org/zerock/ex3/config/CustomSecurityConfig.java
@@ -3,12 +3,21 @@ package org.zerock.ex3.config;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Log4j2
 @Configuration
 public class CustomSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        log.info("filter chain");
+
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
